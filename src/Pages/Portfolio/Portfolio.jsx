624fc2a63cd27a2fd285484039191007d62218cd
Farida Yasmeen <faridayasmeencse@gmail.com>
2024-02-17 11:09:19 +0600

    Portfolio first commit

---
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h2>This is Portfolio</h2>
+        </div>
+    );
+};
+
+export default Portfolio;
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './LayOut/Main';
+import Portfolio from './Pages/Portfolio/Portfolio';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Portfolio></Portfolio>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
